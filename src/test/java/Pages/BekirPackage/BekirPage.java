465161474e539928e6e722ddc38021e7bd968bc7
bekir canverdi <bekircanverdi@gmx.de>
2020-12-02 21:27:55 +0100

    Test 2

--- src/test/java/Pages/BekirPackage/BekirPage.java
+++ src/test/java/Pages/BekirPackage/BekirPage.java
@@ -3,7 +3,7 @@ package Pages.BekirPackage;
 public class BekirPage {
 
     public static void main(String[] args) {
-        System.out.println("Deneme");
+        System.out.println("Deneme...");
     }
 
 }
